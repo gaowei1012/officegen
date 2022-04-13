8398d5308f15e0f163bb6d45f9a55d5baeaa0031
gaowei1012 <gyb2020018@163.com>
2022-04-13 13:52:33 +0800

    添加支持ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: mingwei
+ * @Date: 2022-04-13 13:51:33
+ * @LastEditors: mingwei
+ * @LastEditTime: 2022-04-13 13:51:33
+ * @FilePath: /officegen/index.d.ts
+ * @Description: 
+ */
+
+declare module 'officegen'
